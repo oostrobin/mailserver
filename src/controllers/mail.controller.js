9e35fb27a00bf9439c09b1541d15fbc377b8b9c0
Robin Oost <oostrobin@protonmail.com>
2024-01-09 15:59:15 +0100

    Update email recipient in mail controller

--- src/controllers/mail.controller.js
+++ src/controllers/mail.controller.js
@@ -4,8 +4,8 @@ import dotenv from '../config/dotenv.config.js';
 const sendContactMessage = (req, res) => {
   const { name, email, message, subject } = req.body;
   const mailData = {
-    from: process.env.MAIL_FROM,
-    to: process.env.MAIL_FROM,
+    from: "info@zorgeloosbesparen.nl",
+    to: "info@zorgeloosbesparen.nl",
     subject: 'Nieuw bericht van contactformulier',
     text: `From: ${name} Email: ${email} Message: ${message}`,
     html: `<p>From: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p><p>Subject: ${subject}</p>`,
