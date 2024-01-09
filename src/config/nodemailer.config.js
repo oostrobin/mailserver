b955a1a815f847bb66a5f9ce91fb89c592a3d5a7
Robin Oost <oostrobin@protonmail.com>
2024-01-09 15:56:30 +0100

    Update nodemailer configuration

--- src/config/nodemailer.config.js
+++ src/config/nodemailer.config.js
@@ -1,12 +1,11 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
+  host: "smtp-relay.brevo.com",
   secure: false,
   auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASSWORD,
+    user: "oostrobin1989@gmail.com",
+    pass: "2rOLxadmbK4VRUSD",
   },
 });
 
